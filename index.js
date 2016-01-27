470f9126db02658e6553b33921d6a0765ae25cc6
Dominik Steiner <dominik.j.steiner@gmail.com>
2016-01-27 15:01:51 -0600

    add non sdk xsi property for easy creation

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-module.exports = {model: require('./lib/models/xsi')};
+module.exports = {
+	model: require('./lib/models/xsi'),
+	xsi: require('./lib/models/xsi').create({
+		debug: require('bdsft-sdk-debug').model.create({name: 'xsi'}), 
+		request: require('bdsft-sdk-request').model.create({
+			debug: require('bdsft-sdk-debug').model.create({name: 'request'})
+		})
+	})
+};
